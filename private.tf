0a901118169ec853226aa8d520f0f9b04df61355
Marcelo Collyer <marcelocollyer@gmail.com>
2024-05-30 18:55:00 -0400

    feat: adding route table and associations

--- private.tf
+++ private.tf
@@ -1,30 +1,3 @@
-resource "aws_subnet" "eks_subnet_public_1a" {
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = merge(
-    local.tags,
-    {
-      Name = "${var.project_name}_subnet_public_1a"
-    }
-  )
-}
-
-resource "aws_subnet" "eks_subnet_public_1b" {
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-
-  tags = merge(
-    local.tags,
-    {
-      Name = "${var.project_name}_subnet_public_1b"
-    }
-  )
-}
 
 resource "aws_subnet" "eks_subnet_private_1a" {
   vpc_id            = aws_vpc.eks_vpc.id
---
+++ public.tf
@@ -0,0 +1,55 @@
+resource "aws_subnet" "eks_subnet_public_1a" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}_subnet_public_1a"
+      "kubernetes.io/role/internal-elb" = 1
+    }
+  )
+}
+
+resource "aws_subnet" "eks_subnet_public_1b" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}_subnet_public_1b"
+      "kubernetes.io/role/internal-elb" = 1
+    }
+  )
+}
+
+resource "aws_route_table" "eks_public_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-public-route-table"
+    }
+  )
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_1a" {
+  subnet_id      = aws_subnet.eks_subnet_public_1a.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_1b" {
+  subnet_id      = aws_subnet.eks_subnet_public_1b.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
