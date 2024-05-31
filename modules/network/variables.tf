9a816455ccdf942bebb8034fd3db3b0f37f4c401
Marcelo Collyer <marcelocollyer@gmail.com>
2024-05-31 14:42:50 -0400

    feat: moving network resources to module

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,10 +1,8 @@
 variable "cidr_block" {
-  default     = "10.0.0.0/16"
   type        = string
   description = "Networkking CIDR block to be used for the VPC"
 }
 variable "project_name" {
-  default     = "eksproject"
   type        = string
   description = "Name of the project"
 }
