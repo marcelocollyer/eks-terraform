eee11f4f6d4106331d4618b374bac6b65c050a44
Marcelo Collyer <marcelocollyer@gmail.com>
2024-05-31 14:09:46 -0400

    feat: adding nat gateway and route table associations

--- ngw.tf
+++ ngw.tf
@@ -24,7 +24,7 @@ resource "aws_nat_gateway" "eks_ngw_1a" {
   allocation_id = aws_eip.eks_ngw_eip_1a.id
   subnet_id     = aws_subnet.eks_subnet_public_1a.id
 
-tags = merge(
+  tags = merge(
     local.tags,
     {
       Name = "${var.project_name}-ngw-1a"
@@ -36,10 +36,42 @@ resource "aws_nat_gateway" "eks_ngw_1b" {
   allocation_id = aws_eip.eks_ngw_eip_1b.id
   subnet_id     = aws_subnet.eks_subnet_public_1b.id
 
-tags = merge(
+  tags = merge(
     local.tags,
     {
       Name = "${var.project_name}-ngw-1b"
+    }
+  )
+}
+
+resource "aws_route_table" "eks_private_route_table_1a" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.eks_ngw_1a.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-private-route-table-1a"
+    }
+  )
+}
+
+resource "aws_route_table" "eks_private_route_table_1b" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.eks_ngw_1b.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-private-route-table-1b"
     }
   )
 }
--- public.tf
+++ public.tf
@@ -28,22 +28,6 @@ resource "aws_subnet" "eks_subnet_public_1b" {
   )
 }
 
-resource "aws_route_table" "eks_public_route_table" {
-  vpc_id = aws_vpc.eks_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.eks_igw.id
-  }
-
-  tags = merge(
-    local.tags,
-    {
-      Name = "${var.project_name}-public-route-table"
-    }
-  )
-}
-
 resource "aws_route_table_association" "eks_rtb_assoc_1a" {
   subnet_id      = aws_subnet.eks_subnet_public_1a.id
   route_table_id = aws_route_table.eks_public_route_table.id
