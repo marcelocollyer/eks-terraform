e759cf48089b19909f29da9a653a8e4f85ee6a77
Marcelo Collyer <marcelocollyer@gmail.com>
2024-06-12 23:09:50 -0400

    adding managed node group on private subnet

---
+++ modules/managed-node-group/variables.tf
@@ -0,0 +1,25 @@
+variable "project_name" {
+  default     = "eksproject"
+  type        = string
+  description = "Name of the project"
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Tags to be added to AWS resources"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster name to create MNG"
+}
+
+variable "subnet_private_1a" {
+  type        = string
+  description = "Private subnet ID for AZ 1a"
+}
+
+variable "subnet_private_1b" {
+  type        = string
+  description = "Private subnet ID for AZ 1b"
+}
